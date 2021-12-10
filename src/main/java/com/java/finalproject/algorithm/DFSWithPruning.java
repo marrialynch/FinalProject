67ca11e3584495526fcc23651e36b15996476742
lidgz <li.dongz@northeastern.edu>
2021-12-09 17:21:46 -0800

    fix bugs in DFS Pruning

--- src/main/java/com/java/finalproject/algorithm/DFSWithPruning.java
+++ src/main/java/com/java/finalproject/algorithm/DFSWithPruning.java
@@ -1,46 +1,44 @@
 package com.java.finalproject.algorithm;
 
+
 import java.util.Arrays;
 
 public class DFSWithPruning {
 
-    static int INF = 1000001;
-    static int ans = INF;
+    static int ans;
+    public static int findCheapestPrice(int n, int[][] flights, int src, int dst, int K)
+    {
+            if (n == 0 || flights == null || flights.length == 0)
+                return -1;
+            int[] visited = new int[n];
+            Arrays.fill(visited, 0);
+            visited[src]=1;
 
-    public static int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-        if (n == 0 || flights == null || flights.length == 0)
-            return -1;
-
-        int[] visited = new int[n];
-        Arrays.fill(visited, 0);
-        visited[src]=1;
-
-
-        dfs( flights, src, dst, K + 1, 0, visited);
-        return ans == INF ? -1 : ans;
-    }
-
-    private static void dfs(int[][] flights, int i, int dst, int k, int cost, int[] visited) {
-        if (k < 0) {
-            return;
+            ans =Integer.MAX_VALUE;
+            dfs(flights, src, dst,K+1,0, visited);
+            return ans ==Integer.MAX_VALUE?-1: ans;
         }
-
-        if (i == dst) {
-            ans = cost;
-            return;
-        }
-
-        for (int[] flight : flights) {
-            if (flight[0] == i) {
-                if (visited[flight[1]] == 1)
-                    continue;
-                if (cost + flight[2] > ans)
-                    continue;
-                visited[flight[1]] = 1;
-                dfs(flights, flight[1], dst, k-1, cost + flight[2], visited);
-                visited[flight[1]] = 0;
+        public static void dfs(int[][] flights, int i, int dst, int k, int cost, int[] visited)
+        {
+            if(k < 0)
+                return;
+            if(i == dst)
+            {
+                ans =cost;
+                return;
+            }
+            
+            for(int[] flight : flights)
+            {
+                if (flight[0] == i) {
+                    if (cost + flight[2] > ans)                      
+                        continue;
+                    if  (visited[flight[1]] == 1)
+                        continue;
+                    visited[flight[1]] = 1;
+                    dfs(flights, flight[1], dst, k - 1, cost + flight[2], visited);
+                    visited[flight[1]] = 0;
+                }
             }
         }
-
-    }
 }
